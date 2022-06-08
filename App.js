913d947a976afdf839902ae592b263644677cef6
skura-00 <105990444+skura-00@users.noreply.github.com>
2022-06-07 22:02:23 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+
+import LinkCard from "./Components/LinkCard";
+import webLink from "./Components/webLink";
+import IntroSpace from "./Components/IntroSpace";
+import Header from "./Components/Header";
+import Welcome from "./Components/Welcome";
+import About from "./Components/About";
+import Pictures from "./Components/Pictures";
+import Works from "./Components/Works";
+import Footer from "./Components/Footer";
+
+// "https://www.sk-my-portfolio.com/website/imgs/simpleCounter.png"
+function App() {    
+
+
+  return (
+    <>
+      <Header />
+      <Welcome />
+      <About />
+      <Pictures />
+      {/* <Works /> */}
+      <Footer />
+    </>
+  );
+}
+
+
+
+
+export default App;
+
